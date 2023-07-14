f094f60fc2d4cfb9ad71744d946338cbfa78775a
Oleksii Sysoiev <bostonleek@gmail.com>
2023-07-14 18:40:03 +0300

    add new icons

---
+++ src/objects/Analytics.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+export const Analytics = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+      <path
+        fillRule="evenodd"
+        d="M24.971 23.12L16.518 2.73V9.46L24.97 23.12zM4.625 29.09l10.8-17.743v14.7l-10.8 3.043zm1.64.193l9.741-2.745 9.41 1.943-19.152.802zM16.504 0h-1.067L3 30l25.5-1.068L16.505 0z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+};
